4123acda07815792f04f0cfd34e8f24473490b4e
kurobane <darkflamemaster663@gmail.com>
2024-06-20 19:18:09 -0300

    make api func validate error

--- gateway/main.go
+++ gateway/main.go
@@ -9,20 +9,30 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
 func main() {
 	listenAddr := flag.String("listenAddr", ":6000", "the listen address of the HTTP server")
 	flag.Parse()
-	http.HandleFunc("/invoice", handleGetInvoice)
+	http.HandleFunc("/invoice", makeApiFunc(handleGetInvoice))
 	logrus.Infof("gateway HTTP server running on port %s", *listenAddr)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
 
-func handleGetInvoice(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]string{"invoice": "bla"})
+func handleGetInvoice(w http.ResponseWriter, r *http.Request) error {
+	return writeJSON(w, http.StatusOK, map[string]string{"invoice": "bla"})
 }
 
 func writeJSON(w http.ResponseWriter, code int, v any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
 	return json.NewEncoder(w).Encode(v)
+}
+
+func makeApiFunc(fn apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			writeJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+	}
 }
