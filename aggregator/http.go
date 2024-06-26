7cad9e0ef4f6e147827f7bf4cbc04c269e96c944
kurobane <darkflamemaster663@gmail.com>
2024-06-26 20:06:46 -0300

    make http metrics handler latency

--- aggregator/http.go
+++ aggregator/http.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -14,6 +15,7 @@ import (
 
 type HTTPMetricHandler struct {
 	reqCounter prometheus.Counter
+	reqLatency prometheus.Histogram
 }
 
 func newHTTPMetricsHandler(reqName string) *HTTPMetricHandler {
@@ -21,14 +23,24 @@ func newHTTPMetricsHandler(reqName string) *HTTPMetricHandler {
 		Namespace: fmt.Sprintf("http_%s_%s", reqName, "request_counter"),
 		Name:      "aggregator",
 	})
+	reqLatency := promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: fmt.Sprintf("http_%s_%s", reqName, "request_latency"),
+		Name:      "aggregator",
+		Buckets:   []float64{0.1, 0.5, 1},
+	})
 
 	return &HTTPMetricHandler{
 		reqCounter: reqCounter,
+		reqLatency: reqLatency,
 	}
 }
 
 func (h *HTTPMetricHandler) instrument(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(start time.Time) {
+			h.reqLatency.Observe(time.Since(start).Seconds())
+		}(time.Now())
+
 		h.reqCounter.Inc()
 		next(w, r)
 	}
